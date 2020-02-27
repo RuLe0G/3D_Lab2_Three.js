38bb5be0984727319f2750134fd426d001fe3069
RuLe0G <khlebnikov2015@yandex.ru>
2020-02-27 14:52:09 +0700

    make planets

--- main1.js
+++ main1.js
@@ -1,93 +1,86 @@
-var container;
-var camera, scene, renderer;
-
-var x=0, y=0, z=0;
+var container; 
+var scene, camera, render;
 
 init();
 animate();
+var planets = [];
 
-function init() 
-{
-    container = document.getElementById( 'container');
+
+
+function init()  {
+    container = document.getElementById( 'container' );
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 4000);
-    camera.position.set(300, 0, 0);
-    camera.lookAt(new THREE.Vector3( 0,  0.0, 0));  
-    renderer = new THREE.WebGLRenderer( { antialias: false} );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setClearColor( 0x00000000, 1);
-    container.appendChild( renderer.domElement );
-    window.addEventListener( 'resize', onWindowResize, false);
-    Star();
-    Sun();
-}
 
-function onWindowResize() 
-{
+    camera = new THREE.PerspectiveCamera(
+                                45, window.innerWidth / window.innerHeight, 1, 4000 );  
+    camera.position.set(400, 100, 100);
+
+    camera.lookAt(new THREE.Vector3( 0,  0.0, 0));
+    
+    renderer = new THREE.WebGLRenderer( { antialias: false } );
+    renderer.setSize( window.innerWidth, window.innerHeight ); 
+    renderer.setClearColor( 0x000000, 1); 
+ 
+    container.appendChild( renderer.domElement ); 
+ 
+     window.addEventListener( 'resize', onWindowResize, false ); 
+
+
+    addSphere(1500,new THREE.Vector3(0,0,0),10,"imgs/starmap.jpg") //star
+    addSphere(25,new THREE.Vector3(0,0,0),10,"imgs/sunmap.jpg") //Sun
+    addSphere(10,new THREE.Vector3(0,0,120),10,"imgs/Planets/earth/earthmap1k.jpg") //earth
+    addSphere(15,new THREE.Vector3(0,0,170),10,"imgs/Planets/mars/mars_1k_color.jpg") //mars
+    addSphere(5,new THREE.Vector3(0,0,50),10,"imgs/Planets/mercury/mercurymap.jpg") //mercury    
+    addSphere(9,new THREE.Vector3(0,0,90),10,"imgs/Planets/venus/venusmap.jpg") //venus
+}
+    
+function onWindowResize()  {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function animate()
-{
-    requestAnimationFrame( animate);
-    render();
+function animate()  {
+    requestAnimationFrame( animate ); 
+    render();    
+} 
+
+function render()  {
+    renderer.render( scene, camera ); 
+} 
+
+
+function movement(){
+    planets.model.position.x = N/2+N*Math.cos(a);
+    planets.model.position.y = N*Math.sin(a);
+
+    var x = N/2+2*N*Math.cos(a);
+    var z = N/2+2*N*Math.sin(a);
+    
 }
 
-function render() 
-{
-    renderer.render( scene, camera );
-}
-
-function Sun()
-{
+function addSphere(r, pos, v, texPath){
     var loader = new THREE.TextureLoader();
-    var geometry = new THREE.SphereGeometry( 10, 32, 32 ); 
-    var tex = loader.load( "imgs/sunmap.jpg" );
+    var geometry = new THREE.SphereGeometry(r, 32, 32);
+
+    //var material = new THREE.MeshBasicMaterial({color: 0x000000});
+    var tex = loader.load( texPath );
     tex.minFilter = THREE.NearestFilter; 
 
     var material = new THREE.MeshBasicMaterial({
             map: tex,
             side: THREE.DoubleSide
         });
-    
-    var sphere = new THREE.Mesh( geometry, material );
 
-    scene.add( sphere );
-    
-    sphere.position.x = x;
-    sphere.position.y = y;
-    sphere.position.z = z; 
-
-    sphere.position.set(x,y,z); 
- 
-    var pos = new THREE.Vector3(x,y,z);
+    var sphere = new THREE.Mesh(geometry, material)
     sphere.position.copy(pos);
-}
+    scene.add(sphere);
 
-function Star()
-{
-    var loader = new THREE.TextureLoader();
-    var geometry = new THREE.SphereGeometry( 1000, 32, 32 ); 
-    var tex = loader.load( "imgs/starmap.jpg" );
-    tex.minFilter = THREE.NearestFilter; 
+//    var planet = {}
+//    planet.model = sphere;
+//   planet.x = pos.x
+//    planet.a = 0;
+//    planet.v = v;
 
-    var material = new THREE.MeshBasicMaterial({
-            map: tex,
-            side: THREE.DoubleSide
-        });
-    
-    var sphere = new THREE.Mesh( geometry, material );
-
-    scene.add( sphere );
-    
-    sphere.position.x = x;
-    sphere.position.y = y;
-    sphere.position.z = z; 
-
-    sphere.position.set(x,y,z); 
- 
-    var pos = new THREE.Vector3(x,y,z);
-    sphere.position.copy(pos);
+//    planets.push(planet);
 }
